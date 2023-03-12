d64aa9c71d13644d2583a7c8b4629dd4d2b51797
1nv3rs3 <1nv3rs3r3v3rs3@proton.me>
2023-03-12 09:42:45 +0000

    Creates filesystems

--- archr3v3rs3.sh
+++ archr3v3rs3.sh
@@ -27,7 +27,7 @@ sed -i 's/^#ParallelDownloads/ParallelDownloads/' /etc/pacman.conf
 pacman -S --noconfirm --needed reflector rsync grub
 cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.backup
 
-reflector -a 48 -c $iso -f 5 -l 20 --sort rate --save /etc/pacman.d/mirrorlist
+reflector -a 48 -c "$iso" -f 5 -l 20 --sort rate --save /etc/pacman.d/mirrorlist
 mkdir /mnt &>/dev/null # Hiding error message if any
 
 pacman -S --noconfirm --needed gptfdisk btrfs-progs glibc # Installing prerequisites
@@ -41,17 +41,45 @@ umount -A --recursive /mnt # Unmount everything before we start
 lsblk
 echo -n "Please select the disk you want to use: [ex: /dev/sda]: "
 read -r DISK
-echo $DISK
-sgdisk -Z ${DISK} # Zap all on disk
-sgdisk -a 2048 -o ${DISK} # New gpt disk 2048 alignment
+echo "$DISK"
+sgdisk -Z "${DISK}" # Zap all on disk
+sgdisk -a 2048 -o "${DISK}" # New gpt disk 2048 alignment
 
 # Creating partitions
-sgdisk -n 1::+1M --typecode=1:ef02 --change-name=1:'BIOSBOOT' ${DISK} # partition 1 (BIOS Boot Partition)
-sgdisk -n 2::+300M --typecode=2:ef00 --change-name=2:'EFIBOOT' ${DISK} # partition 2 (UEFI Boot Partition)
-sgdisk -n 3::-0 --typecode=3:8300 --change-name=3:'ROOT' ${DISK} # partition 3 (Root), default start, remaining
+sgdisk -n 1::+1M --typecode=1:ef02 --change-name=1:'BIOSBOOT' "${DISK}" # Partition 1 (BIOS Boot Partition)
+sgdisk -n 2::+300M --typecode=2:ef00 --change-name=2:'EFIBOOT' "${DISK}" # Partition 2 (UEFI Boot Partition)
+sgdisk -n 3::-0 --typecode=3:8300 --change-name=3:'ROOT' "${DISK}" # Partition 3 (Root), default start, remaining
 if [[ ! -d "/sys/firmware/efi" ]]; then # Checking for bios system
-    sgdisk -A 1:set:2 ${DISK}
+    sgdisk -A 1:set:2 "${DISK}"
 fi
-partprobe ${DISK} # reread partition table to ensure it is correct
+partprobe "${DISK}" # Reread partition table to ensure it is correct
 
-# make filesystems
+echo -ne "
+--------------------------------------------------------
+                  Creating Filesystems...
+--------------------------------------------------------
+"
+if [[ "${DISK}" =~ "nvme" ]]; then
+    partition2=${DISK}p2
+    partition3=${DISK}p3
+else
+    partition2=${DISK}2
+    partition3=${DISK}3
+fi
+
+mkfs.vfat -F32 -n "EFIBOOT" "${partition2}"
+# Enter luks password to cryptsetup and format root partition
+echo -n "${LUKS_PASSWORD}" | cryptsetup -y -v luksFormat "${partition3}" -
+# Open luks container and ROOT will be place holder 
+echo -n "${LUKS_PASSWORD}" | cryptsetup open "${partition3}" ROOT -
+# Now format that container
+mkfs.btrfs -L ROOT "${partition3}"
+# Create subvolumes for btrfs
+mount -t btrfs "${partition3}" /mnt
+subvolumesetup
+# Store uuid of encrypted partition for grub
+echo ENCRYPTED_PARTITION_UUID="$(blkid -s UUID -o value "${partition3}")" >> "$CONFIGS_DIR"/setup.conf
+
+# Mount target
+mkdir -p /mnt/boot/efi
+mount -t vfat -L EFIBOOT /mnt/boot/
